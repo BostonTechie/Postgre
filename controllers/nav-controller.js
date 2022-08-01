32ba10197adabc3d22d252a869753ae176b249a8
Andrew D Urquhart <andrewurq@gmail.com>
2022-08-01 12:57:21 -0400

    :tada: sucessful postman get route

--- controllers/nav-controller.js
+++ controllers/nav-controller.js
@@ -1,3 +1,5 @@
+
+//https://www.youtube.com/watch?v=ldYcgPKEZC8
 const express = require('express')
 const router = express.Router()
 const bodyParser = require("body-parser")
@@ -7,6 +9,7 @@ const pool = require("../db/connectGCP")
 // The get route
 router.get('/', async (req, res) => { 
     try {
+      
       const allData = await pool.query("SELECT * FROM crypto LIMIT 2;")
       res.json(allData.rows)
       
@@ -14,8 +17,22 @@ router.get('/', async (req, res) => {
       console.log("here is your get error nav-controller: ", error)
     }
     finally{
-      //res.render('home')
+      
     }
+
+})
+
+router.get('/2', async (req, res) => { 
+  try {
+    const allData = await pool.query("SELECT * FROM crypto LIMIT 3;")
+    res.json(allData.rows)
+    
+  } catch (error) {
+    console.log("here is your get error nav-controller: ", error)
+  }
+  finally{
+    
+  }
 
 })
 
